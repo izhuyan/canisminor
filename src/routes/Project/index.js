9cc34e4e995dd8dbcbf10f0d382d213e1128881f
canisminor1990 <canisminor1990@gmail.com>
2017-09-01 15:10:18 +0800

    sync

--- src/routes/Project/index.js
+++ src/routes/Project/index.js
@@ -1,12 +1,10 @@
 import './index.scss';
 
 export default ({}) => {
-	const proHeight = (min = 0) => {
-		return {minHeight: `${parseInt(window.innerHeight) - min}px`};
-	};
+
 	return (
 		<div className="project">
-			<div className="dev" style={proHeight(150)}>
+			<div className="dev">
 				<div className="title">Updating</div>
 				<div className="desc">coming soon..</div>
 			</div>
