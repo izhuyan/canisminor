637c3b80a2d3a642b60d8cde0286f56faced7dc8
canisminor1990 <canisminor1990@gmail.com>
2017-09-01 19:15:35 +0800

    sync

--- src/routes/Project/index.js
+++ src/routes/Project/index.js
@@ -2,11 +2,27 @@ import './index.scss';
 
 export default ({}) => {
 
+	const imgLib = (path, count) => {
+		let list = [];
+		for (let i = 1; i <= count; i++) {
+			list.push(<img src={`img/${path}_${i}.png`} width="960"/>);
+		}
+		return list;
+	};
+
 	return (
 		<div className="project">
 			<div className="dev">
-				<div className="title">Updating</div>
-				<div className="desc">coming soon..</div>
+				<div className="title">Sense & pixels</div>
+				<div className="desc">want see more ? coming soon..</div>
+				<div className="pro-title">
+					<span className="num">01</span>熊猫金库
+				</div>
+				{imgLib('p_xm/xm', 10)}
+				<div className="pro-title">
+					<span className="num">02</span>智子
+				</div>
+				{imgLib('p_zz/zz', 11)}
 			</div>
 		</div>
 	);
