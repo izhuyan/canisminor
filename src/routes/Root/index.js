24f23dda8e6a0716feb5d45a20e05f616c23f172
canisminor1990 <canisminor1990@gmail.com>
2017-09-02 20:59:46 +0800

    sync

--- src/routes/Root/index.js
+++ src/routes/Root/index.js
@@ -12,8 +12,7 @@ export default ({location, children}) => {
 				'bg-shape__contact': pathname === '/contact'
 			});
 
-	console.log("         %c", "padding:60px 80px;line-height:160px;background:url('http://canisminor.cc/img/loader.gif') no-repeat;background-size:120px auto;");
-	console.info("Hi there, welcome to %ccanisminor.cc", "color:#62e6ac");
+
 
 	return (
 			<div className="root">
