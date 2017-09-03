57474f5f84c620e077f2f8e7ba4d54184fcd9c71
canisminor1990 <canisminor1990@gmail.com>
2017-09-03 15:13:47 +0800

    sync

--- src/routes/Project/index.js
+++ src/routes/Project/index.js
@@ -1,35 +1,24 @@
-import {Title}from'../../components'
+import {Title,Button}from'../../components'
 import {Collapse} from 'antd';
 import './index.scss';
 
 const Panel = Collapse.Panel;
 
-export default ({}) => {
+const imgLib = (num, title, path, page,footer) => {
+	const list = [];
+	const numTilte = (num.toString().length > 1) ? num : `0${num}`
+	const header = (<div className="pro-title"><span className="num">{numTilte}</span>{title}</div>)
+	for (let i = 1; i <= page; i++) list.push(<img src={`img/p_${path}/${path}_${i}.png`} width="960"/>);
+	return (<Panel key={numTilte} header={header}>{list}{footer}</Panel>)
+};
+
+export default () => {
 
 	const proHeight = (min = 0) => {
 		return {minHeight: `${parseInt(window.innerHeight) - min}px`};
 	};
 
-	let num = 0
-
-	const imgLib = (title, path, count) => {
-		let list = [];
-		num++
-		const numTilte = (num.toString().length > 1) ? num : `0${num}`
-
-		for (let i = 1; i <= count; i++) {
-			list.push(<img src={`img/${path}_${i}.png`} width="960"/>);
-		}
-		return (<Panel
-				key={numTilte}
-				header={
-					<div className="pro-title">
-						<span className="num">{numTilte}</span>
-						{title}
-					</div>
-				}
-		>{list}</Panel>)
-	};
+	let num = 1
 
 	return (
 			<div className="project">
@@ -37,10 +26,10 @@ export default ({}) => {
 				       title="Sense & pixels"
 				       desc="wanna see more ? coming soon..."/>
 				<Collapse defaultActiveKey={['01']}>
-					{imgLib("熊猫金库", 'p_xm/xm', 10)}
-					{imgLib("智子", 'p_zz/zz', 11)}
-					{imgLib("沪江学习", 'p_hj/hj', 10)}
-					{imgLib("须臾映社", 'p_iz/iz', 53)}
+					{imgLib(num++, "熊猫金库", 'xm', 10)}
+					{imgLib(num++, "智子", 'zz', 11,<Button url="http://www.zhizigroup.com/">智子 Wisman</Button>)}
+					{imgLib(num++, "沪江学习", 'hj', 10,<Button url="http://www.hujiang.com/app/hujiang/">沪江学习</Button>)}
+					{imgLib(num++, "须臾映社", 'iz', 53,<Button url="http://instant-zine.lofter.com/">Instant-Zine</Button>)}
 				</Collapse>
 			</div>
 	);
