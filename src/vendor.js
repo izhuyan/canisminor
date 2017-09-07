40ec7c4f08215914931e8a155018f098776e8f3b
canisminor1990 <canisminor1990@gmail.com>
2017-09-07 21:33:23 +0800

    sync

--- src/vendor.js
+++ src/vendor.js
@@ -2,9 +2,12 @@ import 'react'
 import 'react-dom'
 import 'dva'
 import 'dva-loading'
+import 'history'
 import 'react-typist'
 import 'video-react'
+import 'classnames'
+import 'nprogress'
 
+// antd
 import antd from 'antd'
-
 const {Layout, Button} = antd
