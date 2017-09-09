f3a6a865f333c59ddadb657a53647ca1c51b4fe0
canisminor1990 <canisminor1990@gmail.com>
2017-09-09 17:20:26 +0800

    sync

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -5,12 +5,10 @@ import { message } from 'antd';
 export default () => {
 
 	const shareConfig = {
-		title: 'CanisMinor', // 分享标题
-		desc: 'UI/UX Designer & FE Developer', // 分享描述
-		link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-		imgUrl: 'https://canisminor.cc/favicons/share.png', // 分享图标
-		type: '', // 分享类型,music、video或link，不填默认为link
-		dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+		title: 'CanisMinor',
+		desc: 'UI/UX Designer & FE Developer',
+		link: window.location.href,
+		imgUrl: 'https://canisminor.cc/favicons/share.png',
 		success: () => message.success('Share success'),
 		cancel: () => message.error('Share cancel'),
 	}
@@ -34,17 +32,12 @@ export default () => {
 					jsApiList: [ // 必填，需要使用的JS接口列表
 						'onMenuShareTimeline',
 						'onMenuShareAppMessage',
-						'onMenuShareAppMessage',
-						'onMenuShareQQ',
-						'onMenuShareWeibo',
-						'onMenuShareQZone',
+
 					]
 				})
 				wx.onMenuShareTimeline(shareConfig); // 分享到朋友圈
 				wx.onMenuShareAppMessage(shareConfig); // 分享给微信好友
-				wx.onMenuShareQQ(shareConfig); // 分享到QQ
-				wx.onMenuShareWeibo(shareConfig); // 分享到微博
-				wx.onMenuShareQZone(shareConfig); // 分享到QQ空间
+
 			})
 
 
