918ef67c70a12f8a3bddbd886ea73f15d26b54ca
canisminor1990 <canisminor1990@gmail.com>
2017-09-09 18:07:59 +0800

    sync

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -1,14 +1,9 @@
 // import wx from 'weixin-jsapi'
 import fetch from 'dva/fetch';
 import {checkStatus, parseJSON} from "./request"
+const wx = window.wx
 
 export default () => {
-
-	const jsApiList = [
-		'onMenuShareTimeline',
-		'onMenuShareAppMessage'
-	]
-
 	fetch(`https://canisminor.cc/api/wechat`, {
 		method: "POST",
 		headers: {
@@ -18,21 +13,28 @@ export default () => {
 		body: JSON.stringify({url: window.location.href})
 	}).then(checkStatus)
 			.then(parseJSON)
-			.then(data => {
-				window.wx.config({
-					debug: true,
-					appId: data.appid, // 必填，公众号的唯一标识
-					timestamp: data.timestamp, // 必填，生成签名的时间戳
-					nonceStr: data.nonceStr, // 必填，生成签名的随机串
-					signature: data.signature, // 必填，签名，见附录1
-					jsApiList: jsApiList
-				})
-				setShareInfo()
-			})
+			.then(data => configWX(data.appid, data.timestamp, data.nonceStr, data.signature))
+}
+
+export function configWX(appId, timestamp, nonceStr, signature) {
+	const jsApiList = [
+		'onMenuShareTimeline',
+		'onMenuShareAppMessage'
+	]
+
+	wx.config({
+		debug: false,
+		appId: appId,
+		timestamp: timestamp,
+		nonceStr: nonceStr,
+		signature: signature,
+		jsApiList: jsApiList
+	})
+
+	setShareInfo()
 }
 
 export function setShareInfo() {
-	let wx = window.wx
 
 	const shareConfig = {
 		title: 'CanisMinor',
