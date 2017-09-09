1ff45340ffa2ba0549f9e6f127f991e5b4f9fab4
canisminor1990 <canisminor1990@gmail.com>
2017-09-09 18:46:06 +0800

    sync

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -2,22 +2,9 @@
 import fetch from 'dva/fetch';
 import {checkStatus, parseJSON} from "./request"
 
-export default () => {
-	fetch(`https://canisminor.cc/api/wechat`, {
-		method: "POST",
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({url: location.href.split('#')[0]})
-	}).then(checkStatus)
-			.then(parseJSON)
-			.then(data => configWX(data))
-}
-
 function configWX(data) {
 	window.wx.config({
-		debug: true,
+		debug: false,
 		appId: "wx8418a1c9c6dd04a3",
 		timestamp: data.timestamp,
 		nonceStr: data.nonceStr,
@@ -31,13 +18,11 @@ function configWX(data) {
 		            'uploadImage',
 		            'previewImage',]
 	})
-
 	setShareInfo()
 }
 
 function setShareInfo() {
-
-	const shareConfig = {
+	const shareOpt = {
 		title: 'CanisMinor',
 		desc: 'UI/UX Designer & FE Developer',
 		link: window.location.href,
@@ -46,19 +31,19 @@ function setShareInfo() {
 
 	window.wx.ready(() => {
 		window.wx.onMenuShareTimeline({
-			title: shareConfig.title,
-			link: shareConfig.link,
-			imgUrl: shareConfig.imgUrl,
+			title: shareOpt.title,
+			link: shareOpt.link,
+			imgUrl: shareOpt.imgUrl,
 			success() {
 			},
 			cancel() {
 			}
 		});
 		window.wx.onMenuShareAppMessage({
-			title: shareConfig.title,
-			desc: shareConfig.desc,
-			link: shareConfig.link,
-			imgUrl: shareConfig.imgUrl,
+			title: shareOpt.title,
+			desc: shareOpt.desc,
+			link: shareOpt.link,
+			imgUrl: shareOpt.imgUrl,
 			type: 'link',
 			dataUrl: '',
 			success() {
@@ -69,4 +54,19 @@ function setShareInfo() {
 	})
 }
 
+export default () => {
+	const fetchOpt = {
+		method: "POST",
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({url: location.href.split('#')[0]})
+	}
 
+	fetch(`https://canisminor.cc/api/wechat`, fetchOpt)
+			.then(checkStatus)
+			.then(parseJSON)
+			.then(data => configWX(data))
+			.catch(err => console.error(err));
+}
