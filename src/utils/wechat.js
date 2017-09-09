96f936da2dd996e19306f267f00dbf357d49f59f
canisminor1990 <canisminor1990@gmail.com>
2017-09-09 16:29:36 +0800

    sync

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -36,17 +36,17 @@ fetch(`https://canisminor.cc/api/wechat`, {
 					'onMenuShareWeibo',
 				]
 			})
+			wx.ready(() => {
+				console.log("ready")
+				wx.onMenuShareTimeline(shareConfig); // 分享到朋友圈
+				wx.onMenuShareAppMessage(shareConfig); // 分享给微信好友
+				wx.onMenuShareQQ(shareConfig); // 分享到QQ
+				wx.onMenuShareWeibo(shareConfig); // 分享到微博
+				wx.onMenuShareQZone(shareConfig); // 分享到QQ空间
+			})
+
+			wx.error((res) => {
+				console.error(res)
+			});
 		})
 
-wx.ready(() => {
-	console.log("ready")
-	wx.onMenuShareTimeline(shareConfig); // 分享到朋友圈
-	wx.onMenuShareAppMessage(shareConfig); // 分享给微信好友
-	wx.onMenuShareQQ(shareConfig); // 分享到QQ
-	wx.onMenuShareWeibo(shareConfig); // 分享到微博
-	wx.onMenuShareQZone(shareConfig); // 分享到QQ空间
-})
-
-wx.error((res) => {
-	console.error(res)
-});
