9cef1ea748a344da40bd383163cc79ae1c357612
canisminor <i@canisminor.cc>
2017-09-20 18:35:47 +0800

    sync

--- src/routes/BlogPage/index.js
+++ src/routes/BlogPage/index.js
@@ -1,22 +1,28 @@
-import {connect} from 'dva';
-import {Markdown,Comment} from '../../components'
-import styles from './index.scss'
+import { Icon, Spin } from 'antd';
+import { connect } from 'dva';
+import { Link } from 'dva/router';
+import { Comment, Markdown } from '../../components';
+import styles from './index.scss';
 
 function mapStateToProps(state) {
 	return {
-		loading: state.loading.global,
-		blogToc: state.blogToc,
+		loading : state.loading.global,
+		blogToc : state.blogToc,
 		blogPage: state.blogPage
 	};
 }
 
-export default connect(mapStateToProps)(({loading,blogToc,blogPage}) => {
+export default connect(mapStateToProps)(({loading, blogToc, blogPage}) => {
 	let data;
-	if (!loading) data = blogPage
+	if (!loading) data = blogPage;
 	return (
-			<div className={styles.page}>
-				<Markdown data={data}/>
-				<Comment />
-			</div>
+		<div className={styles.page}>
+			{loading
+				? <Spin spinning={loading} size="large" style={{width: '100%', lineHeight: '720px'}}/>
+				: <Markdown data={blogPage}/>
+			}
+			<Link to="/blog" className={styles.back}><Icon type="left"/> Go Back</Link>
+			<Comment/>
+		</div>
 	);
 });
