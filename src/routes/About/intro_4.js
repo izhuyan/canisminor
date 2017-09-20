65cc120e32eacd7a0eb39c776b9a4e28a71b4e38
canisminor <i@canisminor.cc>
2017-09-20 19:18:06 +0800

    sync

--- src/routes/About/intro_4.js
+++ src/routes/About/intro_4.js
@@ -1,31 +1,45 @@
-import { AwardList, DocTitle, Showcase,Icon } from '../../components';
+import React from 'react';
+import { AwardList, DocTitle, Icon, Showcase } from '../../components';
 import { awardList, homeToc } from '../../config';
 import styles from './intro_4.scss';
 
-export default () => {
-	let showcaseData = [
-		{title: 'MAJOR:', desc: 'Visual Design'},
-		{title: 'GPA:', desc: '3.58/4'},
-		{title: 'IELTS:', desc: '6.5/9'}
-	];
+export default class extends React.Component {
 
-	return (
-		<div className={styles.intro}>
-			<DocTitle content={homeToc[1]}/>
-			<div className={styles.school}>
-				<Icon className={styles.logo} type="caa-logo"/>
-				<div className={styles.content}>
-					<Icon className={styles.title} type="caa-text"/>
-					<div className={styles.desc}>
-						China Academy of Art<br/>
-						Top Art School World Ranking: 7th
+	constructor(props) {
+		super(props);
+		this.state   = {showAwards: false};
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onClick() {
+		this.setState({showAwards: !this.state.showAwards});
+	}
+
+	render() {
+		let showcaseData = [
+			{title: 'MAJOR:', desc: 'Visual Design'},
+			{title: 'GPA:', desc: '3.58/4'},
+			{title: 'IELTS:', desc: '6.5/9'}
+		];
+
+		return (
+			<div className={styles.intro}>
+				<DocTitle content={homeToc[1]}/>
+				<div className={styles.school}>
+					<Icon className={styles.logo} type="caa-logo"/>
+					<div className={styles.content}>
+						<Icon className={styles.title} type="caa-text"/>
+						<div className={styles.desc}>
+							China Academy of Art<br/>
+							Top Art School World Ranking: 7th
+						</div>
 					</div>
 				</div>
+				<Showcase content={showcaseData}/>
+				<div className={styles.awards}>
+					<AwardList content={awardList}/>
+				</div>
 			</div>
-			<Showcase content={showcaseData}/>
-			<div className={styles.awards}>
-				<AwardList content={awardList}/>
-			</div>
-		</div>
-	);
+		);
+	}
 };
