449a50ec3e7974546a0bb42fc4b781347e5ed981
canisminor <i@canisminor.cc>
2017-09-21 11:14:09 +0800

    sync

--- src/utils/baiduPush.js
+++ src/utils/baiduPush.js
@@ -16,9 +16,9 @@ const BaiduZhanzhang = () => {
 };
 
 const BaiduTongji = () => {
-	var _hmt = _hmt || [];
-	const hm = document.createElement('script');
-	hm.src   = 'https://hm.baidu.com/hm.js?f801b0b6f344318a503552f12bcd55c7';
-	const s  = document.getElementsByTagName('script')[0];
+	const hm = document.createElement("script");
+	hm.src = "https://hm.baidu.com/hm.js?7085397f155d2c882d7a996e5042b14d";
+	const s = document.getElementsByTagName("script")[0];
 	s.parentNode.insertBefore(hm, s);
+
 };
