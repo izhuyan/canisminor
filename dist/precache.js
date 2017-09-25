b698a1ed46ab3d330f1ce3907cffcf6c78eb6a5f
Canis Minor <i@canisminor.cc>
2017-09-25 22:52:57 +0800

    code no warp

--- dist/precache.js
+++ dist/precache.js
@@ -39,7 +39,7 @@
 
 var precacheConfig = [
   ['common.25a110f6.css', '25a110f6f8434d1e9737bb563790818c'],
-  ['common.7771204dd2ceb0b5e280.js', '14c9ef09ce20ed8069e5afe243bb626b'],
+  ['common.de07c5eda7ddb1549d5c.js', '14c9ef09ce20ed8069e5afe243bb626b'],
   ['favicons/android-chrome-192x192.png', '0701c735171badf204c9f2b2846b6107'],
   ['favicons/android-chrome-256x256.png', 'd5bb0f033fc146bdc4a5f00279f251ed'],
   ['favicons/apple-touch-icon.png', 'e65ea0a45b194e770e86fbdcee3f9b24'],
