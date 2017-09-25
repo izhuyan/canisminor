975aa1776708c54c7006ed89cb11bf8b2f4923e6
Canis Minor <i@canisminor.cc>
2017-09-25 22:21:48 +0800

    fix backtop

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,82 +1,60 @@
-import cssnano from 'cssnano';
+import csswring from 'csswring';
 import pxtorem from 'postcss-pxtorem';
 
 export default {
-	entry              : {
-		'index' : './src/index.js',
-		'common': './src/vendor.js'
-	},
-	multipage          : true,
-	publicPath         : '/',
-	disableCSSModules  : false,
-	hash               : true,
-	sass               : {
-		sourceMap   : process.env.NODE_ENV === 'development',
-		includePaths: [
-			'node_modules',
-			'src/style'
-		]
-	},
-	theme              : 'src/style/theme.js',
-	proxy              : {
-		'/api': {
-			target      : 'https://canisminor.cc',
-			changeOrigin: true,
-			xfwd        : true,
-			secure      : true
-		}
-	},
-	extraPostCSSPlugins: [
-		pxtorem({
-			        rootValue    : 16,
-			        propWhiteList: []
-		        })
-	],
-	extraBabelPlugins  : [
-		'transform-runtime',
-		'lodash',
-		[
-			'import',
-			[
-				{
-					libraryName: 'antd',
-					style      : true
-				}
-			]
-		]
-	],
-	autoprefixer       : {
-		browsers: [
-			'iOS >= 8',
-			'Android >= 4'
-		]
-	},
-	env                : {
-		development: {
-			extraBabelPlugins: [
-				'dva-hmr'
-			]
-		},
-		production : {
-			extraPostCSSPlugins: [
-				cssnano({
-					        preset: [
-						        'default',
-						        {discardComments: {removeAll: true}}
-					        ]
-				        })
-			]
-		}
-	},
-	dllPlugin          : {
-		exclude: [
-			'babel-runtime'
-		],
-		include: [
-			'dva/router',
-			'dva/saga',
-			'dva/fetch'
-		]
-	}
+  entry: {
+    index: './src/index.js',
+    common: './src/vendor.js',
+  },
+  multipage: true,
+  publicPath: '/',
+  disableCSSModules: false,
+  hash: true,
+  sass: {
+    sourceMap: process.env.NODE_ENV === 'development',
+    includePaths: ['node_modules', 'src/style'],
+  },
+  theme: 'src/style/theme.js',
+  proxy: {
+    '/api': {
+      target: 'https://canisminor.cc',
+      changeOrigin: true,
+      xfwd: true,
+      secure: true,
+    },
+  },
+  extraPostCSSPlugins: [
+    pxtorem({
+      rootValue: 16,
+      propWhiteList: [],
+    }),
+  ],
+  extraBabelPlugins: [
+    'transform-runtime',
+    'lodash',
+    [
+      'import',
+      [
+        {
+          libraryName: 'antd',
+          style: true,
+        },
+      ],
+    ],
+  ],
+  autoprefixer: {
+    browsers: ['iOS >= 8', 'Android >= 4'],
+  },
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+    production: {
+      extraPostCSSPlugins: [csswring()],
+    },
+  },
+  dllPlugin: {
+    exclude: ['babel-runtime'],
+    include: ['dva/router', 'dva/saga', 'dva/fetch'],
+  },
 };
-
