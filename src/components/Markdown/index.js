6d4c08cb9149cffadff5567c307ea81ccaadd732
canisminor <i@canisminor.cc>
2017-09-27 13:14:55 +0800

    fix md

--- src/components/Markdown/index.js
+++ src/components/Markdown/index.js
@@ -46,7 +46,6 @@ marked.setOptions({
 
 export default ({ data = { title: '', body: '', filename: '' }, ...other }) => {
   const body = data.body;
-  console.log(marked(body));
   return (
     <div {...other}>
       {window.location.pathname === '/blog' ? (
