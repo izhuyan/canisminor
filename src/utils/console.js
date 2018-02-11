4aeae3d2e3dbda55fba57e21e215ae24b09f981e
canisminor1990 <canisminor1990@gmail.com>
2018-02-11 12:20:11 +0800

    sync

--- src/utils/console.js
+++ src/utils/console.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default () => {
   console.log(
-    '%c',
-    "padding:60px 80px;line-height:160px;background:url('http://qn.canisminor.cc/2018-02-11-icon-1.png') no-repeat;background-size:120px auto;",
+    '         %c',
+    "padding:60px 80px;line-height:160px;background:url('http://qn.canisminor.cc/2018-02-11-icon-1.png') no-repeat;background-size:120px auto;background-positon:center;",
   );
   console.info('Hi there, welcome to %ccanisminor.cc', 'color:#62e6ac');
 };
