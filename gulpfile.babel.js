49790a9f47c4097dd9d8c92c600f5b990eb918a2
canisminor1990 <canisminor1990@gmail.com>
2018-02-27 12:27:14 +0800

    sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,25 @@
 import gulp from 'gulp';
+import fs from 'fs-extra';
 import htmlbeautify from 'gulp-html-beautify';
 import swPrecache from 'sw-precache';
 import img from './scripts/img';
+import * as config from './src/config';
 
 gulp.task('opt:img', img);
 
-gulp.task('opt:html', () =>
-  gulp
-    .src('./dist/*.html')
-    .pipe(htmlbeautify({ indentSize: 2 }))
-    .pipe(gulp.dest('./dist/'))
+gulp.task('build:raw', () => fs.writeFileSync('dist/raw-data', JSON.stringify(config)));
+
+gulp.task('opt:html', ['build:raw'], () =>
+	gulp
+		.src('./dist/*.html')
+		.pipe(htmlbeautify({indentSize: 2}))
+		.pipe(gulp.dest('./dist/'))
 );
 
 gulp.task('build:sw', () =>
-  swPrecache.write(`dist/sw.js`, {
-    stripPrefix: 'dist/',
-    staticFileGlobs: ['dist/img/**/*', 'dist/favicons/*', 'dist/font/*', 'dist/*vendor*'],
-    dontCacheBustUrlsMatching: /./,
-  })
+	swPrecache.write(`dist/sw.js`, {
+		stripPrefix              : 'dist/',
+		staticFileGlobs          : ['dist/img/**/*', 'dist/favicons/*', 'dist/font/*', 'dist/*vendor*'],
+		dontCacheBustUrlsMatching: /./
+	})
 );
